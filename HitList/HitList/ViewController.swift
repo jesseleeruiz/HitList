b0a981acf390496f058062f5c724959cbc46eec6
jesseleeruiz <jesseleeruiz@gmail.com>
2020-06-15 08:24:36 -1000

    Added Person Entity. Updated View Controller to save names to disk.

--- HitList/HitList/ViewController.swift
+++ HitList/HitList/ViewController.swift
@@ -7,11 +7,12 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
     
     // MARK: - Properties
-    var names: [String] = []
+    var people: [NSManagedObject] = []
     
     // MARK: - Outlets
     @IBOutlet weak var tableView: UITableView!
@@ -31,7 +32,7 @@ class ViewController: UIViewController {
                                        style: .default) { [weak self] action in
                                         guard let textField = alert.textFields?.first,
                                             let nameToSave = textField.text else { return }
-                                        self?.names.append(nameToSave)
+                                        self?.save(name: nameToSave)
                                         self?.tableView.reloadData()
         }
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
@@ -42,16 +43,39 @@ class ViewController: UIViewController {
         
         present(alert, animated: true)
     }
+    
+    func save(name: String) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        
+        //1
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        //2
+        let entity = NSEntityDescription.entity(forEntityName: "Person", in: managedContext)!
+        let person = NSManagedObject(entity: entity, insertInto: managedContext)
+        
+        //3
+        person.setValue(name, forKey: "name")
+        
+        //4
+        do {
+            try managedContext.save()
+            people.append(person)
+        } catch let error as NSError {
+            NSLog("Could not save. \(error), \(error.userInfo)")
+        }
+    }
 }
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return names.count
+        return people.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let person = people[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = names[indexPath.row]
+        cell.textLabel?.text = person.value(forKey: "name") as? String
         return cell
     }
 }
